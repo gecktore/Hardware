8d7977d849f951abdabafc170e7c6b287baecec7
Danil Gareev <gecktore00@mail.ru>
2023-10-13 14:59:59 +0300

    bd

---
+++ Hardware/Pages/userControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Hardware.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для userControl.xaml
+    /// </summary>
+    public partial class userControl : Page
+    {
+        public userControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
