f6b6248990648be7f7d50e8af6d4cf4e84542c69
Danil Gareev <gecktore00@mail.ru>
2023-10-27 14:56:34 +0300

    up

--- Hardware/Components/Service.cs
+++ Hardware/Components/Service.cs
@@ -10,59 +10,59 @@ namespace Hardware.Components
 {
     public partial class Service
     {
-        public decimal CostDiscount
-        {
-            get
-            {
-                if (Discount == 0)
-                {
-                    return Cost;
-                }
-                else
-                {
-                    return Cost - (Cost * (decimal)Discount / 100);
-                }
-            }
-        }
-        public string costTimeStr
-        {
-            get
-            {
-                if (Discount == 0)
-                    return $"{Cost} рублей за {DurationInSeconds / 60} минут";
-                else
-                    return $" {CostDiscount:0} рублей за {DurationInSeconds / 60} минут";
-            }
-        }
-        public Visibility CostVisibility
-        {
-            get
-            {
-                if (Discount == 0)
-                    return Visibility.Collapsed;
-                else
-                    return Visibility.Visible;
-            }
-        }
-        public string DiscountStr
-        {
-            get
-            {
-                if (Discount == 0)
-                    return "";
-                else
-                    return $"* скидка {Discount}%";
-            }
-        }
-        public Brush ColorDiscount
-        {
-            get
-            {
-                if (Discount == 0)
-                    return new SolidColorBrush(Colors.White);
-                else
-                    return new SolidColorBrush(Colors.LightGreen);
-            }
-        }
+        //public decimal CostDiscount
+        //{
+        //    get
+        //    {
+        //        if (Discount == 0)
+        //        {
+        //            return Cost;
+        //        }
+        //        else
+        //        {
+        //            return Cost - (Cost * (decimal)Discount / 100);
+        //        }
+        //    }
+        //}
+        //public string costTimeStr
+        //{
+        //    get
+        //    {
+        //        if (Discount == 0)
+        //            return $"{Cost} рублей за {DurationInSeconds / 60} минут";
+        //        else
+        //            return $" {CostDiscount:0} рублей за {DurationInSeconds / 60} минут";
+        //    }
+        //}
+        //public Visibility CostVisibility
+        //{
+        //    get
+        //    {
+        //        if (Discount == 0)
+        //            return Visibility.Collapsed;
+        //        else
+        //            return Visibility.Visible;
+        //    }
+        //}
+        //public string DiscountStr
+        //{
+        //    get
+        //    {
+        //        if (Discount == 0)
+        //            return "";
+        //        else
+        //            return $"* скидка {Discount}%";
+        //    }
+        //}
+        //public Brush ColorDiscount
+        //{
+        //    get
+        //    {
+        //        if (Discount == 0)
+        //            return new SolidColorBrush(Colors.White);
+        //        else
+        //            return new SolidColorBrush(Colors.LightGreen);
+        //    }
+        //}
     }
 }
